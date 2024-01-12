e02a091f901823255449cea30a0ddfee02f5266f
leleviola <lelev2001@gmail.com>
2024-01-12 18:51:08 +0100

    sdasc

--- scripts/nodeB.py
+++ scripts/nodeB.py
@@ -4,7 +4,6 @@ import assignment_2_2023.msg
 import sys
 from assignment_2_2023.srv import GoalPos, GoalPosResponse
 
-
 def callback(req):
     return GoalPosResponse(req.x, req.y)
 
@@ -19,13 +18,9 @@ def take_goal(msg):
     print(y)
 
 if '__init__' == '__main__':
-    try:
-        
-        rospy.init_node('nodeB') # init the node
-        #rospy.Service('/goal_pos', GoalPos, callback)
-        rospy.Service("nodeB", GoalPos, callback)
-        #[x, y] = get_last_goal() # take the last goal
-        rospy.Subscriber("/reaching_goal/goal", assignment_2_2023.msg.PlanningGoal, take_goal)
-        rospy.spin() # keep the node running
-    except rospy.ROSInterruptException:
-        print("program interrupted before completion", file=sys.stderr)
+    rospy.init_node('nodeB') # init the node
+    #rospy.Service('/goal_pos', GoalPos, callback)
+    rospy.Service("nodeB", GoalPos, callback)
+    #[x, y] = get_last_goal() # take the last goal
+    rospy.Subscriber("/reaching_goal/goal", assignment_2_2023.msg.PlanningGoal, take_goal)
+    rospy.spin() # keep the node running
