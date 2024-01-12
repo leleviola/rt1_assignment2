1530c6501dc2ba29b6870ce7bbb7d63181a120e5
leleviola <lelev2001@gmail.com>
2024-01-12 20:26:30 +0000

    reviewing node B

--- scripts/nodeB.py
+++ scripts/nodeB.py
@@ -4,23 +4,24 @@ import assignment_2_2023.msg
 import sys
 from assignment_2_2023.srv import GoalPos, GoalPosResponse
 
+x = 0
+y = 0
+
 def callback(req):
-    return GoalPosResponse(req.x, req.y)
+    global x
+    global y
+    return GoalPosResponse(x, y)
 
 def take_goal(msg):
     global x 
     global y
     x = msg.target_pose.pose.position.x
     y = msg.target_pose.pose.position.y
-    print("x: ")
-    print(x)
-    print("y: ")
-    print(y)
 
-if '__init__' == '__main__':
+if __name__ == '__main__':
     rospy.init_node('nodeB') # init the node
     #rospy.Service('/goal_pos', GoalPos, callback)
     rospy.Service("nodeB", GoalPos, callback)
     #[x, y] = get_last_goal() # take the last goal
-    rospy.Subscriber("/reaching_goal/goal", assignment_2_2023.msg.PlanningGoal, take_goal)
+    rospy.Subscriber("/reaching_goal", assignment_2_2023.msg.PlanningGoal, take_goal)
     rospy.spin() # keep the node running
