32dcc881c326077473d3d79385c2abc52ad6676c
leleviola <lelev2001@gmail.com>
2024-01-13 01:07:28 +0000

    manca launchfile

--- scripts/nodeB.py
+++ scripts/nodeB.py
@@ -6,21 +6,7 @@ from assignment_2_2023.srv import GoalPos, GoalPosResponse
 
 x = 0
 y = 0
-def send_goal_position_request(x, y):
-    rospy.wait_for_service('nodeC_service')  # Replace with the actual service name in nodeC
 
-    try:
-        # Create a service proxy
-        service_proxy = rospy.ServiceProxy('nodeC_service', DisAvg)
-
-        # Make the service request
-        response = service_proxy(x, y)
-
-        # Process the response (if needed)
-        process_response(response)
-
-    except rospy.ServiceException as e:
-        rospy.logerr(f"Service call failed: {e}")
         
 def callback(req):
     global x
@@ -37,5 +23,4 @@ def take_goal(msg):
 if __name__ == '__main__':
     rospy.init_node('nodeB') # init the node
     rospy.Service("nodeB", GoalPos, callback) # initializes the server   
-     
     rospy.spin() # keep the node running
--- scripts/nodeC_sub.py
+++ scripts/nodeC_sub.py
@@ -3,7 +3,7 @@ import rospy
 import assignment_2_2023.msg
 import sys
 from assignment_2_2023.srv import GoalPos, GoalPosResponse
-from addignment_2_2023.srv import DisAvg, DisAvgResponse
+from assignment_2_2023.srv import DisAvg, DisAvgResponse
 from assignment_2_2023.msg import PosVel
 import math
 
@@ -11,23 +11,35 @@ x = 0
 y = 0
 vx = 0
 vy = 0
-
-def calc_dist_avg(req)
-    gx = req.x
-    gy = req.y
-    dist = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
-    print(gx)
-    print(gy)
+summx = 0
+summy = 0
+count = 0
+def calc_dist_avg(req):
+    # this was a my idea, that I didn't implemented because I was not sure it would have respected the scope of the assignment, but I think that it could be a good thing to use node b as a client and node c as the server that elaborates the last goal x and y
+    #gx = req.goal_x
+    #gy = req.goal_y
+    gx=rospy.get_param('des_pos_x')
+    gy=rospy.get_param('des_pos_y')
+    dist = math.sqrt((gx - x)**2 + (gy - y)**2)
+    avg_vx = summx / count
+    avg_vy = summy / count
+    return DisAvgResponse(dist, avg_vx, avg_vy)
+    
     
 def retrieve_pos_vel(msg):
+    global summx
+    global summy
+    global count
     x = msg.x
     y = msg.x
     vx = msg.vx
     vy = msg.vy
+    summx = summx + vx
+    summy = summy + vy
+    count = count +1
     
 if __name__ == '__main__':
     rospy.init_node('nodeC_sub')
-    rospy.Service("nodeC", DisAvg, calc_dist_avg)
-    
     rospy.Subscriber('/pos_vel', PosVel, retrieve_pos_vel)
+    rospy.Service("nodeC", DisAvg, calc_dist_avg)
     rospy.spin()
